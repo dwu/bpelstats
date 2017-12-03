f913756aa25dbbf886c2ac289cb2dadb30abe941
Daniel Luebke <github@daniel-luebke.de>
2017-12-03 19:19:54 +0100

    Remove tests with local test data

--- src/test/java/de/danielluebke/bpelstats/metrics/sublanguagestats/XSLTHalsteadMetricsCalculatorTest.java
+++ src/test/java/de/danielluebke/bpelstats/metrics/sublanguagestats/XSLTHalsteadMetricsCalculatorTest.java
@@ -14,41 +14,4 @@ import org.xml.sax.XMLReader;
 
 public class XSLTHalsteadMetricsCalculatorTest {
 
-	@Test
-	public void test() throws Exception {
-		SAXParserFactory spf = SAXParserFactory.newInstance();
-		spf.setNamespaceAware(true);
-		SAXParser saxParser = spf.newSAXParser();
-		XMLReader xmlReader = saxParser.getXMLReader();
-		XSLTHalsteadMetricsCalculator calculator = new XSLTHalsteadMetricsCalculator();
-		xmlReader.setContentHandler(calculator);
-		xmlReader.parse(new InputSource(new FileInputStream("C:/data/workspaces/terravis-av92/ch.terravis.egvt.process.egvt2.1/src/xsl/createCession.xsl")));
-		
-		HalsteadMetrics halsteadMetrics = calculator.getHalsteadMetrics();
-		System.out.println(halsteadMetrics.getTotalNumberOfOperand());
-		System.out.println(halsteadMetrics.getUniqueOperands());
-		
-		System.out.println(halsteadMetrics.getTotalNumberOfOperator());
-		System.out.println(halsteadMetrics.getUniqueOperators());
-	}
-
-	@Test
-	public void testPrintAll() throws Exception {
-		File directory = new File("C:/temp/terravis-xslt");
-		
-		System.out.println("Filename;" + HalsteadMetrics.getCsvHeader());
-		
-		SAXParserFactory spf = SAXParserFactory.newInstance();
-		spf.setNamespaceAware(true);
-		SAXParser saxParser = spf.newSAXParser();
-		XMLReader xmlReader = saxParser.getXMLReader();
-		XSLTHalsteadMetricsCalculator calculator = new XSLTHalsteadMetricsCalculator();
-		xmlReader.setContentHandler(calculator);
-		for(File f : directory.listFiles()) {
-			xmlReader.parse(new InputSource(new FileInputStream(f)));
-			
-			HalsteadMetrics halsteadMetrics = calculator.getHalsteadMetrics();
-			System.out.println(f + ";" + halsteadMetrics.getCsvString());
-		}
-	}
 }
